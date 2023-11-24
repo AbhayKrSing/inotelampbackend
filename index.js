3a0765ba0d49bd094ae3ff1553688a023bd8691d
Abhay Kumar Singh <118283656+AbhayKrSing@users.noreply.github.com>
2023-11-24 20:10:09 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const MongoConnect = require('./db.js')
 const path = require('path');
 var cors = require('cors')
 const app = express()
-require('dotenv').config({ path: path.resolve(__dirname, './.env') });
 app.use(cors())
+require('dotenv').config({ path: path.resolve(__dirname, './.env') });
 app.use(express.json())
 app.use('/api/auth', require('./routes/Auth.js'))
 app.use('/api/notes', require('./routes/Notes.js'))
@@ -14,5 +14,5 @@ app.get('/', (req, res) => {
     res.send('hello')
 })
 app.listen(process.env.PORT || 5000, () => {
-    console.log('listening backend at port 5000')
+    console.log('listening backend at port' + (process.env.PORT || 5000))
 })
